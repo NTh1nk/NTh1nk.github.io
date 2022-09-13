4cb4f987eb1449b0fcb0ecab38a299d3be4ceaef
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2022-09-13 14:17:06 +0200

    Update Pr16.js

--- ProjectEuler/Pr16.js
+++ ProjectEuler/Pr16.js
@@ -1,13 +1,13 @@
     var num = 2**1000
     var ListNum = Array.from(String(num), Number);
     var Total  = 0;
-    console.log(ListNum);
-    Total = parseInt(ListNum[3]) + parseInt(ListNum[4]);
-    console.log(Total);
+        console.log(ListNum);
+        Total = parseInt(ListNum[3]) + parseInt(ListNum[4]);
+        console.log(Total);
   
     for (let i = 0; i > 5 ; i++){
-    Total += parseInt(ListNum[i]);
-    console.log(Total+"Is the total");
+        Total += parseInt(ListNum[i]);
+        console.log(Total+"Is the total");
     }
   
 //  console.log(parseInt(ListNum[3]));
